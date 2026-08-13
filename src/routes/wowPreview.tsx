5844ec90216850fc124ed8c1f315076887311e71
Chico1711 <256874684+realsyncdynamics-spec@users.noreply.github.com>
2026-08-13 04:13:01 +0200

    feat: add wow preview route entry

---
+++ src/routes/wowPreview.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PreviewSelectionPage from '../unified-entry/pages/PreviewSelectionPage';
+
+export function WowPreviewRoute() {
+  return <PreviewSelectionPage />;
+}

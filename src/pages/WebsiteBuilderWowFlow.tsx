2f3009e7ad73f717ef28f0056c48e7c1d07b886b
Chico1711 <256874684+realsyncdynamics-spec@users.noreply.github.com>
2026-08-13 04:13:07 +0200

    feat: add wow-first website builder flow

---
+++ src/pages/WebsiteBuilderWowFlow.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import PreviewSelectionPage from '../unified-entry/pages/PreviewSelectionPage';
+
+export default function WebsiteBuilderWowFlow() {
+  return <PreviewSelectionPage />;
+}
